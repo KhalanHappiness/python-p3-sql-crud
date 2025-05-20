47d1c6ea51531392ce6d0102ff4b5a1be496bc09
happiness <happinesskhalan@gmail.com>
2025-05-20 22:44:02 +0300

    CRUD operations complete

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,9 @@
+-- SQL file to insert new cats into the cats table
+
+-- Insert first cat: Lil' Bub
+INSERT INTO cats (name, age, breed) 
+VALUES ('Lil'' Bub', 5, 'American Shorthair');
+
+-- Insert second cat: Hannah
+INSERT INTO cats (name, age, breed) 
+VALUES ('Hannah', 1, 'Tabby');
